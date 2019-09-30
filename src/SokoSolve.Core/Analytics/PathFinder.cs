882bd8b8fce33e932137a9832e4baabdbcec0d51
Guy Langston <guylangston@gmail.com>
2019-09-30 15:52:26 +0100

    Misc

--- src/SokoSolve.Core/Analytics/PathFinder.cs
+++ src/SokoSolve.Core/Analytics/PathFinder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using SokoSolve.Core.Common;
 using SokoSolve.Core.Primitives;
 
