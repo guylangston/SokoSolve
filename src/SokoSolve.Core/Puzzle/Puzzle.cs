1af81070ba3c5e0f0632ad66ded8f77de8744eeb
Guy Langston <guylangston@gmail.com>
2019-10-14 16:31:14 +0100

    Why is cell a char again?

--- src/SokoSolve.Core/Puzzle/Puzzle.cs
+++ src/SokoSolve.Core/Puzzle/Puzzle.cs
@@ -6,8 +6,37 @@ using System.Text;
 using SokoSolve.Core.Library;
 using SokoSolve.Core.Primitives;
 
-namespace SokoSolve.Core.PuzzleLogic
+namespace SokoSolve.Core.Puzzle
 {
+    public enum PuzzlePiece
+    {
+        // Static
+        Void     = 0b0000_0001,
+        Wall     = 0b0000_0010,
+        Floor    = 0b0000_0100,
+        Goal     = 0b0000_1000,
+        
+        // Dynamic
+        Crate     = 0b0001_0000,
+        Player    = 0b0010_0000,
+    }
+
+    [Flags ]
+    public enum CellState
+    {
+        // Static
+        Void             = 0b0000_0001,
+        Wall             = 0b0000_0010,
+        Floor            = 0b0000_0100,
+        Goal             = 0b0000_1000,
+        
+        // Dynamic
+        CrateFloor       = 0b0001_0100,
+        CrateGoalFloor   = 0b0001_1100,
+        PlayerFloor      = 0b0010_0100,
+        PlayerGoalFloor  = 0b0010_1100,
+    }
+    
     public interface IPosition
     {
         VectorInt2 Position { get; set; }
