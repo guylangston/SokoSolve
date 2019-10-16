054f70ed111f7697391094036489c1d4acc559a3
Guy Langston <guylangston@gmail.com>
2019-10-16 09:34:34 +0100

    Basic but generalised rendering

--- src/SokoSolve.Console/ConsoleGameClient.cs
+++ src/SokoSolve.Console/ConsoleGameClient.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Transactions;
 using ConsoleZ;
+using ConsoleZ.Drawing;
 using ConsoleZ.Win32;
 using SokoSolve.Core.Game;
-using SokoSolve.Core.Primitives;
+using VectorInt2 = SokoSolve.Core.Primitives.VectorInt2;
 
 namespace SokoSolve.Console
 {
     public class SimpleConsoleGameClient : SokobanGameLogic
     {
+        
+        private Dictionary<char, CHAR_INFO_Attr> theme;
+        private Dictionary<char, char> themeChar;
+        private IBufferedAbsConsole<CHAR_INFO> console;
+        private IRenderer<CHAR_INFO> renderer;
+
 
         public void Init()
         {
@@ -18,21 +26,31 @@ namespace SokoSolve.Console
             
             DirectConsole.Setup(80, 25, 7*2, 14*2, "Courier New");
             DirectConsole.Fill(' ', 0);
+            
+            this.console = DirectConsole.Singleton;
+            this.renderer = new ConsoleRendererCHAR_INFO(console);
+            
             Current = Puzzle.Builder.DefaultTestPuzzle();
             
             // TODO: Mouse Interaction https://stackoverflow.com/questions/1944481/console-app-mouse-click-x-y-coordinate-detection-comparison
             
-            theme = new Dictionary<char, byte>()
+            theme = new Dictionary<char, CHAR_INFO_Attr>()
             {
-                {Current.Definition.Void.Underlying,  (byte)ConsoleColor.Black},
-                {Current.Definition.Wall.Underlying,  (byte)((byte)ConsoleColor.Blue + (byte)ConsoleColor.Cyan << 2)},
-                {Current.Definition.Floor.Underlying,  (byte)(ConsoleColor.Gray)},
-                {Current.Definition.Goal.Underlying,  (byte)(ConsoleColor.Blue)},
-                {Current.Definition.Crate.Underlying,  (byte)(ConsoleColor.Yellow)},
-                {Current.Definition.CrateGoal.Underlying,  (byte)(ConsoleColor.Yellow)},
-                {Current.Definition.Player.Underlying,  (byte)(ConsoleColor.White)},
-                {Current.Definition.PlayerGoal.Underlying,  (byte)(ConsoleColor.White)},
+                {Current.Definition.Void.Underlying,  CHAR_INFO_Attr.BACKGROUND_BLUE },
+                {Current.Definition.Wall.Underlying,  CHAR_INFO_Attr.BACKGROUND_GRAY},
+                {Current.Definition.Floor.Underlying, CHAR_INFO_Attr.FOREGROUND_GRAY },
+                {Current.Definition.Goal.Underlying,  CHAR_INFO_Attr.FOREGROUND_GRAY },
+                {Current.Definition.Crate.Underlying, CHAR_INFO_Attr.FOREGROUND_RED |  CHAR_INFO_Attr.FOREGROUND_GREEN | CHAR_INFO_Attr.BACKGROUND_BLUE },
+                {Current.Definition.CrateGoal.Underlying,  CHAR_INFO_Attr.FOREGROUND_RED |  CHAR_INFO_Attr.FOREGROUND_GREEN | CHAR_INFO_Attr.FOREGROUND_INTENSITY},
+                {Current.Definition.Player.Underlying,   CHAR_INFO_Attr.FOREGROUND_RED | CHAR_INFO_Attr.FOREGROUND_INTENSITY},
+                {Current.Definition.PlayerGoal.Underlying,   CHAR_INFO_Attr.FOREGROUND_RED | CHAR_INFO_Attr.FOREGROUND_INTENSITY },
             };
+            themeChar = Current.Definition.ToDictionary(x => x.Underlying, x => x.Underlying);
+            
+            // https://www.fileformat.info/info/unicode/block/box_drawing/list.htm
+            // http://www.fileformat.info/info/unicode/block/block_elements/images.htm
+            themeChar[Current.Definition.Wall.Underlying] = '░';
+            themeChar[Current.Definition.Void.Underlying] = '▓';
         }
         
         public bool Step()
@@ -55,15 +73,17 @@ namespace SokoSolve.Console
             return true;
         }
 
-        private Dictionary<char, byte> theme;
 
         private void Draw()
         {
             foreach (var tile in Current)
             {
-                DirectConsole.Set(tile.Position.X, tile.Position.Y, tile.Cell.Underlying, theme[tile.Cell.Underlying]);
+                renderer[tile.Position.X, tile.Position.Y] = new CHAR_INFO(  themeChar[ tile.Cell.Underlying], theme[tile.Cell.Underlying]);
             }
-            DirectConsole.Update();
+            
+            renderer.Box(new Rect(10, 10, 20, 20), new CHAR_INFO('@') );
+            
+            renderer.Update();
         }
     }
 }
