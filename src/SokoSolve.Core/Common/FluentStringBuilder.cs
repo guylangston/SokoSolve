47fe656ee158ae8124dadf7b29251e22fbe1afa7
Guy Langston <guylangston@gmail.com>
2020-04-06 11:39:39 +0100

    Fluent strings

---
+++ src/SokoSolve.Core/Common/FluentStringBuilder.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Data.Common;
+using System.Text;
+
+namespace SokoSolve.Core.Common
+{
+    public class FluentStringBuilder
+    {
+        StringBuilder sb = new StringBuilder();
+
+        private string sep = ", ";
+
+        public override string ToString() => sb.ToString();
+
+        public FluentStringBuilder Append(string s)
+        {
+            sb.Append(s);
+            return this;
+        }
+        
+        public FluentStringBuilder AppendLine(string s)
+        {
+            sb.AppendLine(s);
+            return this;
+        }
+        
+        public FluentStringBuilder Sep(string s = null)
+        {
+            if (sb.Length > 0) sb.Append(s ?? sep);
+            return this;
+        }
+        
+
+        public FluentStringBuilder IfNotNull<T>(T obj, Func<T, object> whenNotNull)
+        {
+            if (obj is null) return this;
+            
+            sb.Append(whenNotNull(obj)?.ToString());
+            return this;
+        }
+        
+        public FluentStringBuilder If(bool test, Func<object> then)
+        {
+            if (!test) return this;
+            
+            sb.Append(then()?.ToString());
+            return this;
+        }
+        
+        public FluentStringBuilder If(bool test, string then)
+        {
+            if (!test) return this;
+            
+            sb.Append(then);
+            return this;
+        }
+        
+        public FluentStringBuilder When(bool when, Action<FluentStringBuilder> then)
+        {
+            if (when) then(this);
+            return this;
+        }
+        
+        public static  implicit operator string(FluentStringBuilder ff) => ff.ToString();
+        
+       
+    }
+}
--- src/SokoSolve.Core/Solver/SolverStatistics.cs
+++ src/SokoSolve.Core/Solver/SolverStatistics.cs
@@ -12,8 +12,8 @@ namespace SokoSolve.Core.Solver
             Completed = DateTime.MinValue;
         }
 
-        public int TotalNodes     { get; set; } = -1;
-        public bool IsSample { get; set; }
+        public int  TotalNodes { get; set; } = -1;
+        public bool IsSample   { get; set; }
 
         public int TotalDead      { get; set; } = -1;
         public int DepthCompleted { get; set; } = -1;
@@ -34,22 +34,20 @@ namespace SokoSolve.Core.Solver
       
         public string ToString(bool verbose)
         {
-            if (DurationInSec == 0d || TotalNodes == 0)
-            {
-                return $"{TotalNodes,12:#,##0}";
-            }
+            var builder = new FluentStringBuilder()
+                .IfNotNull(Name, x => $"{x,-40}");
             
-            var b = $"{TotalNodes,12:#,##0} nodes at {TotalNodes / DurationInSec,8:#,##0}/s in {Elapased.Humanize()}";
+            if (DurationInSec <= 0d || TotalNodes == 0)
+            {
+                return builder.Append($"{TotalNodes,12:#,##0} nodes");
+            }
 
-            if (verbose)
-            {
-                if (TotalDead >= 0) b += $" D:{TotalDead:#,##0}:{TotalDead * 100 / TotalNodes:0}%";
-                if (Duplicates >= 0) b += $" Dup:{Duplicates:#,##0}:{Duplicates * 100 / TotalNodes:0}%";
-            }
-            
-            return Name == null
-                ? b
-                : $"{Name,-40} {b}";
+            builder.When(verbose, then => 
+               then.If(TotalDead >= 0, () => $" D:{TotalDead:#,##0}:{TotalDead * 100 / TotalNodes:0}%")
+                   .If(Duplicates >= 0, () => $" D:{Duplicates:#,##0}:{Duplicates * 100 / TotalNodes:0}%"))
+            .Append($"{TotalNodes,12:#,##0} nodes at {TotalNodes / DurationInSec,8:#,##0}/s in {Elapased.Humanize()}");
+
+            return builder;
         }
 
         public string ToStringShort() => ToString(false);
