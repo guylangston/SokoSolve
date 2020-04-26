89cfa0b6b95d49a64019bdcee0b659a869a5e522
Guy Langston <guylangston@gmail.com>
2020-04-26 20:18:41 +0100

    Misc

--- src/SokoSolve.Core/Solver/SolverNodeFactoryTrivial.cs
+++ src/SokoSolve.Core/Solver/SolverNodeFactoryTrivial.cs
@@ -9,6 +9,7 @@ namespace SokoSolve.Core.Solver
 {
     public class SolverNodeFactoryTrivial : SolverNodeFactoryBaseDefault
     {
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override SolverNode CreateInstance(SolverNode parent, VectorInt2 player, VectorInt2 push, IBitmap crateMap, IBitmap moveMap) 
             => new SolverNode(parent, player, push, crateMap, moveMap);
