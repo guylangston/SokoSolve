d1a347c63b6d9eb6b74a30fa39bc358ea013777c
Guy Langston <guylangston@gmail.com>
2020-12-05 10:27:26 +0000

    Fixed datas

--- src/SokoSolve.Tests/TestHelper.cs
+++ src/SokoSolve.Tests/TestHelper.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using System.Runtime.Versioning;
 using Xunit;
 
 namespace SokoSolve.Tests
 {
     public static class TestHelper
     {
-        public static string GetDataPath()
+        public static string GetTestProjectPath()
         {
-            if (Environment.CurrentDirectory.EndsWith("src\\SokoSolve.Tests\bin\\Debug\\netcoreapp3.1"))
-                return @"../../../../../data/";
-            
-            if (Environment.CurrentDirectory.EndsWith("src/SokoSolve.Tests/bin/Debug/netcoreapp3.1"))
-                return @"../../../../../data/";
+            var          curr        = Environment.CurrentDirectory;
+            const string projectName = "SokoSolve.Tests";
+            var          i           =curr.IndexOf(projectName, StringComparison.Ordinal);
+            if (i < 0) throw new Exception($"Current Directory must include the project '{projectName}' : {curr}");
 
-            if (Directory.Exists(@"C:\Projects\SokoSolve\")) return @"C:\Projects\SokoSolve\data\";
-
-            throw new Exception($"Unable to find data path. Curr={Environment.CurrentDirectory}");
+            return curr[0..(i + projectName.Length)] + Path.DirectorySeparatorChar;
+        }
+        
+        public static string GetDataPath()  // rel project: ../../data 
+        {
+            var path = Path.Combine(GetTestProjectPath(), "../../data/");
+            if (!Directory.Exists(path)) throw new Exception($"Cannot resolver data path (rel test project): {path}");
+            return path;
         }
 
         public static string GetLibraryPath()
