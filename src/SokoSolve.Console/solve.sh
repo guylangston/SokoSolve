94e994d1f9f59189f06fd29832520fe4428dbbe7
Guy Langston <guylangston@gmail.com>
2020-12-14 12:49:05 +0000

    Cleaning up SolverState

--- src/SokoSolve.Console/solve.sh
+++ src/SokoSolve.Console/solve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 puzzle="${1:-SQ1}"
 solver="${2:-fr!}"
-pool="${3:-bb:lock:bst:lt}"
-min="${4:-bb:lock:bst:lt}"
+pool="${3:-bb:bst:lt}"
+min="${4:-3}"
 args="$puzzle --solver $solver --pool $pool --min $min"
 tfm="netcoreapp5.0"
 dotnet build -c Release -f $tfm -p:WarningLevel=0 
